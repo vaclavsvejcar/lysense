1cdc4d9d5746824409312dba43ad0187b030917d
Vaclav Svejcar <vaclav.svejcar@gmail.com>
2023-11-10 19:59:04 +0100

    Make build info available in runtime

---
+++ src/main/java-templates/com/norcane/lysense/meta/BuildInfo.java
@@ -0,0 +1,8 @@
+package com.norcane.lysense.meta;
+
+public class BuildInfo {
+
+    public static final String NAME = "${project.name}";
+    public static final String DESCRIPTION = "${project.description}";
+    public static final String VERSION = "${project.version}";
+}
--- src/main/java/com/norcane/lysense/meta/ProductInfo.java
+++ src/main/java/com/norcane/lysense/meta/ProductInfo.java
@@ -7,6 +7,10 @@ public final class ProductInfo {
         throw new IllegalStateException("This class is not meant to be instantiated");
     }
 
+    public static final String NAME = BuildInfo.NAME;
+    public static final String DESCRIPTION = BuildInfo.DESCRIPTION;
+    public static final SemVer VERSION = SemVer.from(BuildInfo.VERSION);
+
     public static final String ERROR_CODE_PREFIX = "LSN";
     public static final String USER_CONFIGURATION_FILE = ".lysense.yaml";
 }
