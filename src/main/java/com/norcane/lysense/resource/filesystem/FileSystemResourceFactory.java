b425d1b324734666638d7c1f904cd3cef8b317ea
Vaclav Svejcar <vaclav.svejcar@gmail.com>
2023-11-29 15:12:21 +0100

    Change FileSystemResourceFactory to be IterableResourceFactory

--- src/main/java/com/norcane/lysense/resource/filesystem/FileSystemResourceFactory.java
+++ src/main/java/com/norcane/lysense/resource/filesystem/FileSystemResourceFactory.java
@@ -2,13 +2,33 @@ package com.norcane.lysense.resource.filesystem;
 
 import com.norcane.lysense.resource.Resource;
 import com.norcane.lysense.resource.loader.DefaultResourceFactory;
-import com.norcane.lysense.resource.loader.ResourceFactory;
+import com.norcane.lysense.resource.loader.IterableResourceFactory;
+import com.norcane.lysense.resource.util.PathMatcher;
+import com.norcane.lysense.resource.util.ResourceWalker;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Collections;
+import java.util.List;
+import java.util.function.Predicate;
 
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
 
 @ApplicationScoped
 @DefaultResourceFactory
-public class FileSystemResourceFactory implements ResourceFactory {
+public class FileSystemResourceFactory implements IterableResourceFactory {
+
+    private final PathMatcher pathMatcher;
+    private final ResourceWalker resourceWalker;
+
+    @Inject
+    public FileSystemResourceFactory(PathMatcher pathMatcher,
+                                     ResourceWalker resourceWalker) {
+
+        this.pathMatcher = pathMatcher;
+        this.resourceWalker = resourceWalker;
+    }
 
     @Override
     public Resource.Scheme scheme() {
@@ -18,5 +38,18 @@ public class FileSystemResourceFactory implements ResourceFactory {
     @Override
     public Resource resource(String path) {
         return FileSystemResource.of(path);
+    }
+
+    @Override
+    public List<Resource> resources(String locationGlobPattern, Predicate<Resource> filter) {
+        final String rootPathString = pathMatcher.resolveRootPath(locationGlobPattern);
+        final String pattern = locationGlobPattern.substring(rootPathString.length());
+
+        final Path rootPath = Path.of(rootPathString);
+        if (!Files.exists(rootPath)) {
+            return Collections.emptyList();
+        }
+
+        return resourceWalker.walk(rootPath, pattern, FileSystemResource::of, filter);
     }
 }
--- src/test/java/com/norcane/lysense/resource/filesystem/FileSystemResourceFactoryTest.java
+++ src/test/java/com/norcane/lysense/resource/filesystem/FileSystemResourceFactoryTest.java
@@ -5,14 +5,17 @@ import com.norcane.lysense.resource.loader.DefaultResourceFactory;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
 import java.net.URI;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.List;
 
 import io.quarkus.test.junit.QuarkusTest;
 import jakarta.inject.Inject;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @QuarkusTest
 class FileSystemResourceFactoryTest {
@@ -37,5 +40,37 @@ class FileSystemResourceFactoryTest {
 
         assertEquals(uri, resource.uri());
         assertEquals(content, resource.readAsString());
+    }
+
+    @Test
+    public void resources() throws Exception {
+        final Path tempDirectory = Files.createTempDirectory(null);
+        final Path fileA = Path.of("a.txt");
+        final Path fileB = Path.of("foo/b.txt");
+        Files.createDirectories(tempDirectory.resolve(fileB.getParent()));
+        Files.createFile(tempDirectory.resolve(fileA));
+        Files.createFile(tempDirectory.resolve(fileB));
+
+        final String pattern = tempDirectory + File.separator + "**.txt";
+
+        // check if correctly resolves non-existing paths
+        assertTrue(factory.resources("/not/existing", _ -> true).isEmpty());
+
+        // check if correctly resolves pattern path
+        final List<Resource> resources1 = factory.resources(tempDirectory + "/**", _ -> true);
+        assertEquals(2, resources1.size());
+        assertTrue(resources1.stream().anyMatch(resource -> resource.uri().toString().endsWith("a.txt")));
+        assertTrue(resources1.stream().anyMatch(resource -> resource.uri().toString().endsWith("b.txt")));
+
+        // check if correctly resolves single file path
+        final List<Resource> resources2 = factory.resources(tempDirectory.resolve(fileA).toString(), _ -> true);
+        assertEquals(1, resources2.size());
+        assertTrue(resources2.stream().anyMatch(resource -> resource.uri().toString().endsWith("a.txt")));
+
+        // check if correctly resolves GLOB pattern
+        final List<Resource> resources3 = factory.resources(pattern, _ -> true);
+        assertEquals(2, resources3.size());
+        assertTrue(resources3.stream().anyMatch(resource -> resource.uri().toString().endsWith("a.txt")));
+        assertTrue(resources3.stream().anyMatch(resource -> resource.uri().toString().endsWith("b.txt")));
     }
 }
