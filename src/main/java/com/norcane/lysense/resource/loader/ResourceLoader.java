85343684e549f8fce72485ecc0700b665bff65f5
Vaclav Svejcar <vaclav.svejcar@gmail.com>
2023-11-30 15:36:49 +0100

    Add support to load multiple resources to ResourceLoader

--- src/main/java/com/norcane/lysense/resource/loader/ResourceLoader.java
+++ src/main/java/com/norcane/lysense/resource/loader/ResourceLoader.java
@@ -1,9 +1,13 @@
 package com.norcane.lysense.resource.loader;
 
 import com.norcane.lysense.resource.Resource;
+import com.norcane.lysense.resource.exception.ResourceNotFoundException;
+import com.norcane.lysense.resource.util.PathMatcher;
 
+import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.function.Predicate;
 
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.enterprise.inject.Instance;
@@ -15,21 +19,90 @@ import static com.norcane.toolkit.Prelude.toMap;
 @ApplicationScoped
 public class ResourceLoader {
 
+    private static final String GLOB_SINGLE_DIR = "*";
+    private static final String GLOB_RECURSIVE_DIR = "**";
+
     private final Map<Resource.Scheme, ResourceFactory> factories;
+    private final PathMatcher pathMatcher;
     private final ResourceFactory defaultFactory;
 
     @Inject
     public ResourceLoader(Instance<ResourceFactory> factories,
+                          PathMatcher pathMatcher,
                           @DefaultResourceFactory ResourceFactory defaultFactory) {
 
         this.factories = toMap(ResourceFactory::scheme, factories);
+        this.pathMatcher = pathMatcher;
         this.defaultFactory = defaultFactory;
     }
 
+    /**
+     * Loads single resource specified by its <i>path</i>. Path consists of <i>source ID</i> and <i>path</i> separated by colon (e.g.
+     * {@code classpath:/foo/bar.txt}). If no <i>source ID</i> prefix is used, then {@link ResourceFactory} annotated with {@link DefaultResourceFactory} is
+     * used. No <i>GLOB</i> wildcards or directory paths are allowed, for more flexible resource loading use {@link #resources(String, Predicate, boolean)}.
+     *
+     * @param path location of the resource
+     * @return loaded resource
+     * @throws ResourceNotFoundException if the resource cannot be found or location points to directory
+     */
     public Resource resource(String path) {
         nonNull(path);
 
         return findFactory(path).resource(dropScheme(path));
+    }
+
+    /**
+     * Loads all resources matching the given <i>pattern</i>. Pattern can be either path to concrete file, directory or <i>GLOB</i> pattern. If the location is
+     * directory, then based on {@code listDirectoryRecursively} parameter, either all resources in the directory and subdirectories are recursively returned or
+     * only the ones in the top-level directory.  Location consists of <i>source ID</i> and <i>path</i> separated by colon (e.g.
+     * {@code classpath:/foo/bar.txt}). If no <i>source ID</i> prefix is used, then {@link ResourceFactory} annotated with {@link DefaultResourceFactory} is
+     * used. If no resource is found, then empty list is returned.
+     *
+     * <br><br><b>Given the following directory structure:</b>
+     * <pre>
+     *   resources/
+     *   ├─sub-resources/
+     *   │ └─b.txt
+     *   └─a.txt
+     * </pre>
+     *
+     * <b>Following resources are returned:</b>
+     * {@snippet lang = "java":
+     *   resources("file:resources", _ -> true, false);        // [resources/a.txt]
+     *   resources("file:resources", _ -> true, true);         // [resources/a.txt, resources/sub-resources/b.txt]
+     *   resources("file:resources/a.txt", _ -> true, false);  // [resources/a.txt]
+     *   resources("file:**.txt", _ -> true, false);           // [resources/a.txt, resources/sub-resources/b.txt]
+     *}
+     *
+     * @param pattern file, directory or <i>GLOB</i> pattern
+     * @param filter filter do decide whether to include the resource in the result or not
+     * @param listDirectoryRecursively if the given location is directory, then whether to list all resources in the directory recursively or only the ones in
+     * the top-level directory
+     * @return list of loaded resources
+     */
+    public List<Resource> resources(String pattern, Predicate<Resource> filter, boolean listDirectoryRecursively) {
+        final ResourceFactory factory = findFactory(pattern);
+
+        // fail fast if the factory doesn't support iterating resources
+        if (!(factory instanceof IterableResourceFactory iterableFactory)) {
+            throw new UnsupportedOperationException(STR. "Iterating over resources with scheme '\{ factory.scheme() }' is not supported" );
+        }
+
+        // strip the scheme from the pattern (e.g. 'classpath:' or 'file:')
+        final String patternWithoutScheme = dropScheme(pattern);
+
+        final Resource resource = resourceOrNull(factory, patternWithoutScheme);
+        if (resource != null) {
+            return List.of(resource);
+        }
+
+        // if the pattern points to directory, return recursively all resources in it
+        final String dirSuffix = listDirectoryRecursively ? GLOB_RECURSIVE_DIR : GLOB_SINGLE_DIR;
+        final String patternOrDirectory = pathMatcher.isPattern(patternWithoutScheme)
+                                          ? patternWithoutScheme
+                                          : patternWithoutScheme + "/" + dirSuffix;
+
+        return iterableFactory.resources(patternOrDirectory, filter);
     }
 
     private ResourceFactory findFactory(String path) {
@@ -44,5 +117,13 @@ public class ResourceLoader {
             .filter(scheme -> path.startsWith(scheme.value() + ":"))
             .map(scheme -> path.substring(scheme.value().length() + 1))
             .orElse(path);
+    }
+
+    private Resource resourceOrNull(ResourceFactory factory, String path) {
+        try {
+            return factory.resource(path);
+        } catch (ResourceNotFoundException e) {
+            return null;
+        }
     }
 }
--- src/test/java/com/norcane/lysense/resource/loader/ResourceLoaderTest.java
+++ src/test/java/com/norcane/lysense/resource/loader/ResourceLoaderTest.java
@@ -5,13 +5,18 @@ import com.norcane.lysense.resource.exception.ResourceNotFoundException;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
 import java.net.URI;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
 
 import io.quarkus.test.junit.QuarkusTest;
 import jakarta.inject.Inject;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @QuarkusTest
 class ResourceLoaderTest {
@@ -30,5 +35,37 @@ class ResourceLoaderTest {
 
         // check that exception is thrown for non-file resource
         assertThrows(ResourceNotFoundException.class, () -> resourceLoader.resource("/resources-test"));
+    }
+
+    @Test
+    void resources() throws IOException {
+        final Path tempDirectory = Files.createTempDirectory(null);
+        final Path fileA = Path.of("a.txt");
+        final Path fileB = Path.of("foo/b.txt");
+        Files.createDirectories(tempDirectory.resolve(fileB.getParent()));
+        Files.createFile(tempDirectory.resolve(fileA));
+        Files.createFile(tempDirectory.resolve(fileB));
+
+        // check that all resources from given directory are recursively loaded
+        final List<Resource> resources1 = resourceLoader.resources(tempDirectory.toString(), _ -> true, true);
+        assertEquals(2, resources1.size());
+        assertTrue(resources1.stream().anyMatch(resource -> resource.uri().toString().endsWith("a.txt")));
+        assertTrue(resources1.stream().anyMatch(resource -> resource.uri().toString().endsWith("b.txt")));
+
+        // check that all resources from given directory are recursively loaded when using '**' GLOB
+        final List<Resource> resources2 = resourceLoader.resources(tempDirectory + "/**", _ -> true, true);
+        assertEquals(2, resources2.size());
+        assertTrue(resources2.stream().anyMatch(resource -> resource.uri().toString().endsWith("a.txt")));
+        assertTrue(resources2.stream().anyMatch(resource -> resource.uri().toString().endsWith("b.txt")));
+
+        // check that all resources from given directory are non-recursively loaded
+        final List<Resource> resources3 = resourceLoader.resources(tempDirectory.toString(), _ -> true, false);
+        assertEquals(1, resources3.size());
+        assertTrue(resources3.stream().anyMatch(resource -> resource.uri().toString().endsWith("a.txt")));
+
+        // check that concrete resource is returned when using absolute path
+        final List<Resource> resources4 = resourceLoader.resources(tempDirectory.resolve(fileA).toString(), _ -> true, true);
+        assertEquals(1, resources4.size());
+        assertTrue(resources4.stream().anyMatch(resource -> resource.uri().toString().endsWith("a.txt")));
     }
 }
